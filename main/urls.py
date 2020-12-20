c6fe27730f25b6f3598b403a5ee705ccc9b30942
Ajay Jain <ajayjain1024@gmail.com>
2020-12-21 01:58:11 +0530

    Update urls.py
    Mapped the urls, so If somebody directly opens your website, on "https://127.0.0.1/",  homepage will be displayed.

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from main import views
 
 urlpatterns = [
+    path('',views.homepage),
     path('home/', views.homepage, name='home'),
     path('items/', views.itemspage, name='items'),
     path('login/', views.loginpage, name='login'),
